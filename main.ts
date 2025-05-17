4eaedb2a6f0c4e1b43f6abc97f57af3cd66d0eee
Martin Williams <martinwork@users.noreply.github.com>
2025-05-17 15:29:33 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,34 @@
+/**
+ * To time a low pulse...
+ * 
+ * Wait for high
+ * 
+ * Wait for low
+ * 
+ * Record time of start of pulse
+ * 
+ * Wait for high
+ * 
+ * Record time of end of pulse
+ * 
+ * Calculate time difference
+ */
 let pin = AnalogPin.P0
 let threshold = 800
+while (pins.analogReadPin(pin) < threshold) {
+    images.iconImage(IconNames.No).showImage(0, 0)
+}
 images.iconImage(IconNames.Square).showImage(0)
 while (pins.analogReadPin(pin) >= threshold) {
 	
 }
-let timeLow = input.runningTimeMicros()
+let timeStart = input.runningTimeMicros()
 images.iconImage(IconNames.SmallSquare).showImage(0, 0)
 while (pins.analogReadPin(pin) < threshold) {
 	
 }
-let timeHigh = input.runningTimeMicros()
-let time = timeHigh - timeLow
+let timeEnd = input.runningTimeMicros()
+let time = timeEnd - timeStart
 basic.forever(function () {
     basic.showNumber(time)
     basic.pause(200)
